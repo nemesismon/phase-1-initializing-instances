825d1717dc932132adcaed2e71b15377c21ef99d
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-01-25 11:18:01 -0500

    commit tree bc6e4c2a4dbefe64bcc2e809136979e48f21e19e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+
+class Breakfast {
+    constructor (food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+const bfast = new Breakfast('eggs', 'juice');
+
+class Lunch {
+    constructor (salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+const newLunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea');
+
+class Dinner {
+    #dessert;
+    constructor (salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.dessert = dessert;
+    }
+}
+
+const newDinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake');
